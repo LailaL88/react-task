20aa60f5ff1b556791f2f25693306d372415ba40
LailaL88 <58104146+LailaL88@users.noreply.github.com>
2020-06-21 10:50:04 +0300

    Add files via upload

---
+++ precache-manifest.ea8b5a7f26e09b606aa29d04eeb28469.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfdf56dc8c01678f8f9f68589139c085",
+    "url": "/react-task/index.html"
+  },
+  {
+    "revision": "2afc9a79acacbce7f2ec",
+    "url": "/react-task/static/css/main.60fd27bd.chunk.css"
+  },
+  {
+    "revision": "6090c83908ee6745c657",
+    "url": "/react-task/static/js/2.6475de71.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-task/static/js/2.6475de71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2afc9a79acacbce7f2ec",
+    "url": "/react-task/static/js/main.6122c9cb.chunk.js"
+  },
+  {
+    "revision": "b874c8d85b5a34830c7a",
+    "url": "/react-task/static/js/runtime-main.75086287.js"
+  }
+]);
